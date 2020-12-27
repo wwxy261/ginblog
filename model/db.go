9399568275007b9a8fbd2da9b323ef4e59995e3e
xieyang <xieyang@MacBook-Pro-2.local>
2020-12-27 23:25:37 +0800

    build table and config database:

---
+++ model/db.go
@@ -0,0 +1,30 @@
+package model
+
+import (
+	"fmt"
+	"ginblog/utils"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+var err error
+
+func InitDb() {
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8mb4&parseTime=True&loc=Local",
+		utils.DbUser,
+		utils.DbPassword,
+		utils.DbHost,
+		utils.DbPost,
+		utils.DbName,
+	)
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		fmt.Printf("连接数据库失败，请检查参数: ",err)
+	}
+
+	db.AutoMigrate(&User{},&Category{},&Article{})
+	//db.AutoMigrate(&User{},&Category{})
+}
